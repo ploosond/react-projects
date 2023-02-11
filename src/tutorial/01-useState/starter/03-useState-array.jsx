d55dc74d298ed01ab1757d78410901e536c90264
Prajwol Devkota <prajwol.devkota016@gmail.com>
2023-02-11 20:01:50 +0100

    Completed useState

--- src/tutorial/01-useState/starter/03-useState-array.jsx
+++ src/tutorial/01-useState/starter/03-useState-array.jsx
@@ -1,5 +1,40 @@
+import { useState } from "react";
+import { data } from "../../../data.js";
+
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = useState(data);
+  const removeItem = (id) => {
+    // console.log(id);
+    const newPeople = people.filter((person) => person.id !== id);
+    setPeople(newPeople);
+  };
+  // const clearAllItem = () => {
+  //   setPeople([]);
+  // };
+
+  return (
+    <div>
+      {people.map((person) => {
+        const { id, name } = person;
+        return (
+          <div key={id}>
+            <h4>{name}</h4>
+            <button type='button' onClick={() => removeItem(id)}>
+              remove
+            </button>
+          </div>
+        );
+      })}
+      <button
+        type='button'
+        style={{ marginTop: "2rem" }}
+        className='btn'
+        onClick={() => setPeople([])}
+      >
+        clear items
+      </button>
+    </div>
+  );
 };
 
 export default UseStateArray;
--- src/tutorial/01-useState/starter/04-useState-object.jsx
+++ src/tutorial/01-useState/starter/04-useState-object.jsx
@@ -1,7 +1,29 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState({
+    name: "peter",
+    age: 24,
+    hobby: "read books",
+  });
+
+  const handleChange = () => {
+    //   setPerson({ name: "john", age: 28, hobby: "scream at the computer" });
+    // };
+    // setPerson("shakeAndBake");
+    // setPerson({ name: "Prajwol" });
+    setPerson({ ...person, name: "Prajwol" });
+  };
+  return (
+    <>
+      <h3>{person.name}</h3>
+      <h3>{person.age}</h3>
+      <h3>Enjoys : {person.hobby}</h3>
+      <button type='button' className='btn' onClick={handleChange}>
+        Show john
+      </button>
+    </>
+  );
 };
 
 export default UseStateObject;
